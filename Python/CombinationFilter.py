af94a8effa83abab88b3251126bd51cdb87fffee
garethnisbet <garethnisbet@gmail.com.com>
2018-07-23 16:54:16 +0100

    Added Credits

--- Python/CombinationFilter.py
+++ Python/CombinationFilter.py
@@ -7,7 +7,39 @@ interactive(True)
 v1 = np.loadtxt('T-Bot_FilteredData.dat')
 
 
-#################  Kalman filter variables ###############
+################   Simple Combination Filter    #############
+# Gareth Nisbet 2018
+#############################################################
+'''
+Note the integral of the gyro here differs from T-Bot because of latency;
+possibly because the serial print interferes with the timing. 
+This code serves as a guide to show how the filters work. 
+Typically, the filter_weighting will be a factor of 10 smaller on 
+the T-Bot. A filter_weighting of 0 zero will be equivalent to using
+the gyro only. A value of 1 will be equivalent to using the accelerometer  
+only. You can use serial_GetData.py to collect the data from the T-BOT
+to see how effective your filter is.
+'''
+
+angle = 0
+filter_weighting = 0.13
+
+def getAngleCFilter(pitch, gyro_rate, dt):
+    global angle
+    angle += gyro_rate * dt
+    y = pitch - angle
+    angle += filter_weighting * y
+    return angle
+
+############################################################
+
+
+
+############################################################
+# Kalman Filter - based on code written by Kristian Lauszus, 
+# TKJ Electronics (2012).
+#################  Kalman filter variables #################
+
 Q_angle = 0.15 # process noise 
 Q_bias = 0.03 #
 bias = 0
@@ -18,8 +50,8 @@ R_measure = 1 # measurement noise
 P = np.zeros((2,2))
 K = np.zeros(2)
 
-
-def getAngle(pitch, gyrorate, dt):
+# Kalman filter based on code written by Kristian Lauszus, TKJ Electronics (2012).
+def getAngle(pitch, gyrorate, dt):  
     global bias
     global angle
     global P
@@ -49,29 +81,7 @@ def getAngle(pitch, gyrorate, dt):
     return angle
 
 
-################   Simple Combination Filter    ################
-'''
-Note the integral of the gyro here differs from T-Bot because of latency;
-possibly because the serial print interferes with the timing. 
-This code serves as a guide to show how the filters work. 
-Typically, the filter_weighting will be a factor of 10 smaller on 
-the T-Bot. A filter_weighting of 0 zero will be equivalent to using
-the gyro only. A value of 1 will be equivalent to using the accelerometer  
-only. You can use serial_GetData.py to collect the data from the T-BOT
-to see how effective your filter is.
-'''
 
-angle = 0
-filter_weighting = 0.3
-
-def getAngleCFilter(pitch, gyro_rate, dt):
-    global angle
-    angle += gyro_rate * dt
-    y = pitch - angle
-    angle += filter_weighting * y
-    return angle
-
-##########################################################
 
 angleCF = np.array([getAngleCFilter(v1[x,1],v1[x,2],v1[x,0]) for x in range(v1.shape[0])])
 v1[0,2] = v1[0,4]
@@ -84,17 +94,19 @@ t = np.cumsum(v1[:,0])
 
 ###############   Plot the data  ########################
 
-plt.figure()
+plt.figure(figsize=(12, 3), dpi=80)
+plt.title('Combination Filter')
 ax = plt.subplot(111)
-ax.plot(t, v1[:,1], 'g',label = 'Measured Pitch')
-ax.plot(t, angleCF, 'm',label = 'Combination Filter')
-ax.plot(t, angleKF, 'b',label = 'Kalman Filter')
-ax.plot(t, v1[:,3], 'r',label = 'Filtered by T-Bot')
-ax.plot(t, gyroangle, 'k',label = 'Unfiltered Gyro Angle')
-ax.plot(t, v1[:,4], 'c--',label = 'Unfiltered Gyro Angle from T-Bot')
+ax.plot(t, v1[:,1], 'g',label = 'Accelerometer Angle')
+ax.plot(t, v1[:,4], 'c--',label = 'Gyro Angle')
+#ax.plot(t, angleCF, 'm',label = 'Combination Filter')
+#ax.plot(t, angleKF, 'b',label = 'Kalman Filter')
+ax.plot(t, v1[:,3], 'r',label = 'Combination Filter')
+#ax.plot(t, gyroangle, 'k',label = 'Unfiltered Gyro Angle')
+
 ax.legend(loc = 'best',prop={ 'size': 8})
 plt.xlabel('t (s)')
-plt.ylabel('angle')
+plt.ylabel('angle (deg)')
 plt.axis('tight')
 
 
