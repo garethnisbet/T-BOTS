874e51dd664b071a72091d55d4363b1827bc6022
garethnisbet <garethnisbet@gmail.com.com>
2018-12-28 10:13:18 +0000

    Extra notes for uploading to the Arduino

--- TBot/TBot.ino
+++ TBot/TBot.ino
@@ -75,7 +75,9 @@ PID gyroyPID(&gyroyInput, &gyroyOutput, &gyroySetpoint, gyroKp, gyroKi, gyroKd, 
 
 /////////////////////          Setup Motors             /////////////////////////
 
-const int m1ndb = 23 , m1pdb = 23, m2ndb = 20 , m2pdb = 23;
+// m1 is the T-Bot's right motor, m2 is the left
+
+const int m1ndb = 23 , m1pdb = 23, m2ndb = 20 , m2pdb = 23; // not values are always positive
 const int m2stby = 6, m2ain1 = 4, m2ain2 = 5, m2pwmpin = 9,  mpsfactor = 240;
 
 Motor m1 = Motor(m2ain1, m2ain2, m2stby, m2pwmpin, m1ndb, m1pdb, mpsfactor);
