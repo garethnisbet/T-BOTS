05e4aa90b64e6f412fc69f6c80ad89ec4a4d70ea
garethnisbet <garethnisbet@gmail.com>
2019-04-23 19:51:11 +0100

    Updated auto trim

--- TBot/TBot.ino
+++ TBot/TBot.ino
@@ -87,11 +87,12 @@ SoftwareSerial BTSerial(17,16);  // RX, TX
 
 /////////////////////          Setup Motors             /////////////////////////
 
-// m1 is the T-Bot's right motor, m2 is the left (USE DCMotorCalibrate.ino to find the correct values)
+// m1 is the T-Bot's right motor, m2 is the left
 
-//const int m1ndb = 23 , m1pdb = 23, m2ndb = 23 , m2pdb = 23; // note the values are always positive good for george
+const int m1ndb = 23 , m1pdb = 23, m2ndb = 23 , m2pdb = 23; // note the values are always positive good for george
 //const int m1ndb = 33 , m1pdb = 33, m2ndb = 33 , m2pdb = 33; // note the values are always positive good for B
-const int m1ndb = 40 , m1pdb = 42, m2ndb = 22 , m2pdb = 21; // good fot T-Bot
+//const int m1ndb = 40 , m1pdb = 42, m2ndb = 22 , m2pdb = 21; // good fot T-Bot
+//const int m1ndb = 22 , m1pdb = 25, m2ndb = 22 , m2pdb = 23; // good fot Foxy
 const int m2stby = 6, m2ain1 = 4, m2ain2 = 5, m2pwmpin = 9,  mpsfactor = 240, mpsfactor2 = 240;
 
 Motor m1 = Motor(m2ain1, m2ain2, m2stby, m2pwmpin, m1ndb, m1pdb, mpsfactor);
