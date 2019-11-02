afc39ca5917aaaf198d9b9c3d1245cc7e9f4c5d4
garethnisbet <garethnisbet@gmail.com>
2019-11-02 17:56:19 +0000

    ...

--- TBot/TBot.ino
+++ TBot/TBot.ino
@@ -94,7 +94,7 @@ const int m1ndb = 28 , m1pdb = 28, m2ndb = 28 , m2pdb = 28; // note the values a
 //const int m1ndb = 33 , m1pdb = 33, m2ndb = 33 , m2pdb = 33; // note the values are always positive good for B
 //const int m1ndb = 40 , m1pdb = 42, m2ndb = 22 , m2pdb = 21; // good fot T-Bot
 //const int m1ndb = 22 , m1pdb = 25, m2ndb = 22 , m2pdb = 23; // good fot Foxy
-const int m2stby = 6, m2ain1 = 4, m2ain2 = 5, m2pwmpin = 9,  mpsfactor = 250, mpsfactor2 = 235;
+const int m2stby = 6, m2ain1 = 4, m2ain2 = 5, m2pwmpin = 9,  mpsfactor = 250, mpsfactor2 = 255;
 
 Motor m1 = Motor(m2ain1, m2ain2, m2stby, m2pwmpin, m1ndb, m1pdb, mpsfactor);
 
