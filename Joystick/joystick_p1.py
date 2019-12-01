11634f507452b9fe6ef539979a2f311b38978d96
garethnisbet <garethnisbet@gmail.com>
2019-12-01 21:07:55 +0000

    Restructured

--- Joystick/joystick_p1.py
+++ Joystick/joystick_p1.py
@@ -3,88 +3,22 @@ import pygame, sys, pygame.mixer, os
 from pygame.locals import *
 from time import sleep, time
 import bluetooth as bt
-dirpath = os.path.dirname(os.path.realpath(__file__))
+from Classes import tbt
+
+
+dirpath = os.path.dirname(os.path.realpath(__file__))+'/Images'
 timestart = time()
 speedfactor = 0.6
 speedlimit = 70
 turnspeedlimit = 60
 
-oldvals = [0,0,0,0]
 
+###################  Setup Bluetooth   #############################
+
+oldvals = [0,0,0,0]
 sendcount = 0
 bd_addr = '98:D3:51:FD:81:AC' 
-
-
-class bt_connect(object):
-    def __init__(self,bt_addr,port):
-        self.bt_addr = bt_addr
-        self.port = port
-    def connect(self,con):
-        if con == 1:
-            try:
-                print('connecting to '+self.bt_addr)
-                self.sock = bt.BluetoothSocket( bt.RFCOMM )
-                self.sock.connect((self.bt_addr,1))
-                self.sock.settimeout(5)
-                print('connected to '+self.bt_addr)
-                return self.sock.getpeername()
-            except:
-                print('Connection Failed')
-                
-        else:
-            try:
-                print('Closing connection to '+self.bt_addr)
-                self.sock.close()
-                return 'Disconnected'
-            except:
-                return 'Not connected'
-                
-    def connected(self):
-        try:
-            self.sock.getpeername()
-            status = 1
-        except:
-            status = 0
-        return status
-
-    def send_data(self,sendstr,sendtwice):
-        try:
-            if sendstr == '200200Z':
-                if sendtwice <= 2:
-                    builtstr = chr(0X02)+sendstr+chr(0X03)
-                    self.sock.send(builtstr.encode(encoding='utf-8'))
-                    sendtwice += 1                   
-            else:
-                builtstr = chr(0X02)+sendstr+chr(0X03)
-                self.sock.send(builtstr.encode(encoding='utf-8'))
-                sendtwice = 0
-        except:
-            print('Error sending data...')
-        return sendtwice
-
-    def get_data(self,oldvalues = [0,0,0,0]):
-        try:
-            data = self.sock.recv(32).decode(encoding='utf-8')
-            data = data.split('\x02')
-            ministring = data[0]
-            splitstr = ministring.split(',')
-        except:
-            splitstr = []
-        
-        if len(splitstr) == 4:
-            oldkps, oldkp, oldtrim, oldgyro = splitstr[0], splitstr[1], splitstr[2], splitstr[3]
-            oldgyro = oldgyro[:-2]
-            oldvalues = [oldkps, oldkp, oldtrim, oldgyro]
-            return oldkps, oldkp, oldtrim, float(oldgyro)
-        else:
-            return oldvalues[0], oldvalues[1],oldvalues[2],oldvalues[3]
-            
-    def get_name(self):
-        try:
-            return self.sock.getpeername()
-        except:
-            return 'Not Connected'
-
+btcom = tbt.bt_connect(bd_addr,1)
 
 
 ###################  Screen Text Class #############################
@@ -110,10 +44,9 @@ class TextPrint(object):
     def unindent(self):
         self.x -= 10
 
-
 ###################  Instantiate BT Class #############################    
 
-btcom = bt_connect(bd_addr,1)
+
       
         
 # Define some colors.
---
+++ Joystick/test.py
@@ -0,0 +1,7 @@
+from Classes import tbt
+
+oldvals = [0,0,0,0]
+
+sendcount = 0
+bd_addr = '98:D3:51:FD:81:AC'
+btcom = tbt.bt_connect(bd_addr,1)
