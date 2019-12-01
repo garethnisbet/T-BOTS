4ae6c69533717d2f62d79f23e9c61ecf81f69104
garethnisbet <garethnisbet@gmail.com>
2019-12-01 11:23:01 +0000

    Default

--- TBot/TBot.ino
+++ TBot/TBot.ino
@@ -90,7 +90,7 @@ SoftwareSerial BTSerial(17,16);  // RX, TX
 
 // m1 is the T-Bot's right motor, m2 is the left
 
-const int m1ndb = 28 , m1pdb = 28, m2ndb = 28 , m2pdb = 25; // note the values are always positive good for george
+const int m1ndb = 28 , m1pdb = 28, m2ndb = 28 , m2pdb = 28; // note the values are always positive good for george
 //const int m1ndb = 33 , m1pdb = 33, m2ndb = 33 , m2pdb = 33; // note the values are always positive good for B
 //const int m1ndb = 40 , m1pdb = 42, m2ndb = 22 , m2pdb = 21; // good fot T-Bot
 //const int m1ndb = 22 , m1pdb = 25, m2ndb = 22 , m2pdb = 23; // good fot Foxy
