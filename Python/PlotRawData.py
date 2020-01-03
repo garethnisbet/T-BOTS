324a888ccad66b4cfd6ef6b0998aeb78fcda12a1
garethnisbet <garethnisbet@gmail.com>
2020-01-03 14:23:52 +0000

    ...

---
+++ Python/PlotRawData.py
@@ -0,0 +1,25 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import xlim, ylim
+#plt.ion()
+
+v1 = np.loadtxt('T-Bot_Raw_Data.dat')
+
+plt.figure(figsize=(10, 8))
+xdata = np.arange(v1.shape[0])
+ax = plt.subplot(111)
+plt.title('Raw Data From T-Bot')
+ax.plot(xdata, v1[:,0], c=(255/255.,0/255.,0/255.),label = 'Column 1')
+ax.plot(xdata, v1[:,1], c=(0/255.,255/255.,0/255.),label = 'Column 2')
+ax.plot(xdata, v1[:,2], c=(0/255.,0/255.,255/255.),label = 'Column 3')
+ax.plot(xdata, v1[:,3], c=(100/255.,100/255.,100/255.),label = 'Column 4')
+
+
+plt.xlabel('Data Point')
+plt.ylabel('Arb. Units')
+plt.xlim([xdata.min(),xdata.max()])
+ax.legend(loc = 'best',prop={ 'size': 8})
+plt.grid()
+plt.show()
+plt.savefig('RawData.svg')
+
--- Python/serial_GetUSB_Data.py
+++ Python/serial_GetUSB_Data.py
@@ -18,23 +18,24 @@ import serial
 try:
     ser = serial.Serial('/dev/ttyUSB0', 38400)
 except:
+    print('Trying ttyUSB1')
     ser = serial.Serial('/dev/ttyUSB1', 38400)
 ii=1
 sleep(2)
 t=0
 def serial2v():
-    v1=ser.readline()
+    v1=ser.readline().decode('utf-8')
     a=np.array(list(map(float,v1.split('\t')[:-1])))
     return a
-v1=np.array([[]]*2).T # expecting 5 columns 
+v1=np.array([[]]*4).T # expecting 5 columns 
 for ii in range(200):
     try:
-        v1 = np.vstack([v1,serial2v()])
+        v1 = np.vstack([v1,serial2v()])       
     except:
         v1 = np.vstack([v1,serial2v()])
     print(serial2v())
 
 
-np.savetxt('T-Bot_UltrasoundData.dat',v1)
+np.savetxt('T-Bot_Raw_Data.dat',v1)
 
 
